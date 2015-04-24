bad06c841fbf7dccae972f521f2de3923985bf5e
Amarpreet Minhas <amarpreet@minhas.io>
2015-04-24 16:43:35 -0400

    Fuckery

--- bangbot/plugins/nickserv.py
+++ bangbot/plugins/nickserv.py
@@ -5,6 +5,8 @@ import irc3
 @irc3.event(r':(?P<ns>NickServ)!NickServ@services. NOTICE (?P<nick>irc3) :'
             r'This nickname is registered.*')
 def register(bot, ns=None, nick=None):
+    print('REGISTER')
+    print(bot + ns + nick)
     try:
         password = bot.config[bot.config.host][nick]
     except KeyError:
