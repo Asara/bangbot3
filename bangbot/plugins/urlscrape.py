4e1aebf8aea11809439ab5a7577ed6e9dc4c95c9
Amarpreet Minhas <amarpreet@minhas.io>
2015-05-26 14:27:31 -0400

    Half working urlscrape, added requirements

--- bangbot/plugins/urlscrape.py
+++ bangbot/plugins/urlscrape.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from irc3.plugins.command import command
 import irc3
-
+import re
 
 @irc3.plugin
 class Plugin(object):
@@ -11,9 +11,6 @@ class Plugin(object):
     @irc3.event(irc3.rfc.PRIVMSG)
     def scrape(self, mask, event, target, data):
         """Scrape text for urls"""
-        print('HERE')
         urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', data)
-        print('URLS')
         for url in urls:
-            print('LOOP')
-            yield str(url)
+            print(str(url))
