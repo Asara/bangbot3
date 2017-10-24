cea0b7f833cad71b6a388e9da104fa15adc2a925
Asara <amarpreet@minhas.io>
2017-10-24 13:22:16 +0000

    Renamed cryptocur, added monero and btg

--- bangbot/plugins/cc.py
+++ bangbot/plugins/cc.py
@@ -4,20 +4,31 @@ import irc3
 from coinmarketcap import Market
 from json import loads
 
+price_cheat_sheet = {
+  "btc": "bitcoin",
+  "bch": "bitcoin-cash",
+  "btg": "bitcoin-gold",
+  "eth": "ethereum",
+  "xrp": "ripple",
+  "ltc": "litecoin",
+  "xmr", "monero"
+
+}
+
 @irc3.plugin
 class Plugin(object):
     def __init__(self, bot):
         self.bot = bot
 
     @command(permission=None, options_first=True)
-    def cryptocur(self, mask, target, args):
+    def cc(self, mask, target, args):
         """Get the price of the specified cryptocurrency
 
-            %%cryptocur <requestedCryptoCur>
+            %%cc <requestedCryptoCur>
         """
         try:
             curlookup = Market()
             cryptoCur= str(args['<requestedCryptoCur>'])
-            yield(cryptoCur + ': ' + loads(curlookup.ticker(cryptoCur).decode("utf-8"))[0]["price_usd"])
+            yield(price_cheat_sheet[cryptoCur] + ': ' + loads(curlookup.ticker(price_cheat_sheet[cryptoCur]).decode("utf-8"))[0]["price_usd"])
         except:
             yield('Currency not found')
