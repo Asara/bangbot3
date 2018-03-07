0824e851d6ec9179180d1669631a1e8b0957d02d
Asara <amarpreet@minhas.io>
2018-03-07 21:49:29 +0000

    Added hotsnake on request

--- bangbot/plugins/lit.py
+++ bangbot/plugins/lit.py
@@ -3,11 +3,21 @@ from irc3.plugins.command import command
 import irc3
 import datetime
 
-line = [
+fire = [
     '   )     )     ) ',
     '  ) \   ) \   ) \ ',
     ' / ) ( / ) ( / ) (',
     ' \(_)/ \(_)/ \(_)/',
+]
+
+snake = [
+    ' >---==',
+    '      ==',
+    '      =',
+    '     ==',
+    '    =====',
+    '   =========="',
+
 ]
 @irc3.plugin
 class Plugin(object):
@@ -23,5 +33,17 @@ class Plugin(object):
         cur = 0
         fin = 4
         while cur != fin:
-            yield str(line[cur])
+            yield str(fire[cur])
+            cur += 1
+
+    @command(permission=None, options_first=True)
+    def hotsnake(self, mask, target, args):
+        """SNEK
+
+            %%hotsnake
+        """
+        cur = 0
+        fin = 6
+        while cur != fin:
+            yield str(snake[cur])
             cur += 1
